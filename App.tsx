19fdf22bf7da42523677063481fbd2d43b2ba355
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-29 00:40:49 -0400

    Added navigation for teh entire app without authetication flow.

--- App.tsx
+++ App.tsx
@@ -1,29 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { primaryTheme }  from './src/theme/paper.theme'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
-const Stack = createNativeStackNavigator();
+import { PicLinkNavigation } from './src/navigation';
+ 
 
 
 export default function App() {
   return (
     <PaperProvider theme={primaryTheme}>
       <SafeAreaProvider>
-      {/* <NavigationContainer>
-        <Stack.Navigator> */}
-      
-        <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-        {/* </Stack.Navigator>
-
-      </NavigationContainer> */}
+        <PicLinkNavigation/>
       </SafeAreaProvider>
       </PaperProvider>
 
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import { AppNavigator } from './AppNavigator';
-import { AccountNavigator } from './AccountNavigator';
+import { AppNavigator } from './appNavigator';
+// import { AccountNavigator } from './AccountNavigator';
 
+// Make sure to add SignIn and Signup Navigator later along with firebase. 
 
-export const Navigation = () => {
+export const PicLinkNavigation = () => {
 
   return (
     <NavigationContainer>
