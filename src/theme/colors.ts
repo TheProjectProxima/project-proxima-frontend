7d99064f07cce24a166a7b816af070129281a7ab
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-30 01:41:37 -0400

    Added all theme for project.

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,39 +1,29 @@
-colors: {
-    primary: '#034748',
-    accent: '#11B5E4',
-    background: '#FFFFFF',
-    surface: '#FFFFFF',
-    text: '#001021',
-    error: '#B71F0E',
-    disabled: '#BEC6C6',
-    placeholder: '#1481BA',
-    backdrop: '#001021',
-  },
+// fix color object for ui and text later on 
 
-  export const colors = {
+export const colors = {
     brand: {
-      primary: '#9009AE',
-      secondary: '#2C4DDD',
-      tertiary: '#EA6ATF',
+        primary: '#9009AE',
+        secondary: '#2C4DDD',
+        tertiary: '#EA6ATF',
     },
     ui: {
-      primary: '#262626',
-      secondary: '#757575',
-      tertiary: '#F1F1F1',
-      quaternary: '#FFFFFF',
-      disabled: '#DEDEDE',
-      error: '#D0421B',
-      success: '#138000',
+        primary: '#262626',
+        secondary: '#757575',
+        tertiary: '#F1F1F1',
+        quaternary: '#FFFFFF',
+        disabled: '#DEDEDE',
+        error: '#D0421B',
+        success: '#138000',
     },
     bg: {
-      primary: '#000000',
+        primary: '#000000',
     },
     text: {
-      primary: '#262626',
-      secondary: '#757575',
-      disabled: '#9C9C9C',
-      inverse: '#FFFFFF',
-      error: '#D0421B',
-      success: '#138000',
+        primary: '#262626',
+        secondary: '#757575',
+        disabled: '#9C9C9C',
+        inverse: '#FFFFFF',
+        error: '#D0421B',
+        success: '#138000',
     },
-  };
+};
