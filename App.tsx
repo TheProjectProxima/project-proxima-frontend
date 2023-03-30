64e8e19cf49ac689e4dfb20d1f4437a2e8e27415
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-30 02:02:21 -0400

    Added expo fonts for nunito sans.

--- App.tsx
+++ App.tsx
@@ -2,19 +2,33 @@ import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { primaryTheme }  from './src/theme/paper.theme'
 import { PicLinkNavigation } from './src/navigation';
- 
-
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from '@expo-google-fonts/nunito-sans';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return (
-    <PaperProvider theme={primaryTheme}>
-      <SafeAreaProvider>
-        <PicLinkNavigation/>
-      </SafeAreaProvider>
-      </PaperProvider>
-
-
-  ); 
+  const [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+  if (!fontsLoaded) {
+    return (<AppLoading/>)
+  }
+  else {
+    return (
+      <PaperProvider theme={primaryTheme}>
+        <SafeAreaProvider>
+          <PicLinkNavigation/>
+        </SafeAreaProvider>
+        </PaperProvider>
+  
+  
+    ); 
+  }
+ 
 }
 
 
