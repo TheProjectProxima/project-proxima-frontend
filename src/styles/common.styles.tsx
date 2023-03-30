50a7ca168339c1c4b20574535f83e14d210af748
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-30 18:20:02 -0400

    Added basic common styled components.

--- src/styles/common.styles.tsx
+++ src/styles/common.styles.tsx
@@ -2,14 +2,37 @@
 
 import React from 'react';
 import { Image, View, Platform } from 'react-native';
+import { Card } from 'react-native-paper';
 import styled from 'styled-components/native';
+import { colors } from '../theme/colors';
 
 const StyledCompactImage = styled(Image)`
-  border-radius: 10px;
-  width: 50px;
-  height: 50px;
-`;
+  border-radius: 0.625em;
+  width: 3.125em;
+  height: 3.125em;
+`
 
-const CompactCard = styled(View)`
+const StyledCompactCard = styled(Card)`
+  background-color: ${colors.bg.primary};
+  width: 6.25em;
+  height: 6.25em;
+`
 
-`;
+const StyledImage = styled(Image)`
+  border-radius: 0.625em;
+  width: 
+`
+
+const StyledCard = styled(Card)`
+  background-color: ${colors.bg.primary};
+  width: 95%;
+  align-self: center;
+`
+
+const StyledIcon = styled(Image)`
+  width: 1.25em;
+  height: 1.25em;
+`
+
+
+
