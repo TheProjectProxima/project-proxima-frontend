9bae36c0e0d41fdf4d46a2c49d191ecef37b02fa
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-30 01:38:39 -0400

    Added colors and fonts object.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet} from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { primaryTheme }  from './src/theme/paper.theme'
@@ -15,16 +14,7 @@ export default function App() {
       </PaperProvider>
 
 
-  );
+  ); 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 
