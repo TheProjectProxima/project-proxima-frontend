2d19eb798443cf718384b8f949f079d3842edbcd
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-03-30 18:01:16 -0400

    Added base folder srtucture for styled components.

---
+++ src/styles/common.styles.tsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import { Image, View, Platform } from 'react-native';
+import styled from 'styled-components/native';
+
+const StyledCompactImage = styled(Image)`
+  border-radius: 10px;
+  width: 50px;
+  height: 50px;
+`;
+
+const CompactCard = styled(View)`
+
+`;
