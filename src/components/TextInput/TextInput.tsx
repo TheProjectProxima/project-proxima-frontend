8e625060d4b0a48fe9f627ad2e772ec7384c3456
samblock <block.sa@northeastern.edu>
2023-04-25 21:56:40 -0400

    starting login screen

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,43 +1,37 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { TextInput as Input } from 'react-native-paper';
-
 import { theme } from '../../theme';
 
-export default function TextBox({ errorText, description, ...props } : {errorText:any, description:any}) {
-    return (
-      <View style={styles.container}>
-        <Input
-          style={styles.input}
-          selectionColor={theme.colors.text.primary}
-          underlineColor="transparent"
-          mode="outlined"
-          {...props}
-        />
-        {description && !errorText ? (
-          <Text style={styles.description}>{description}</Text>
-        ) : null}
-        {errorText ? <Text style={styles.error}>{errorText}</Text> : null}
-      </View>
-    )
-  }
-  
-  const styles = StyleSheet.create({
-    container: {
-      width: '100%',
-      marginVertical: 12,
-    },
-    input: {
-      backgroundColor: theme.colors.bg.primary,
-    },
-    description: {
-      fontSize: 13,
-      color: theme.colors.text.secondary,
-      paddingTop: 8,
-    },
-    error: {
-      fontSize: 13,
-      color: theme.colors.text.error,
-      paddingTop: 8,
-    },
-  })
+type Props = React.ComponentProps<typeof Input> & { errorText?: string };
+
+const TextInput = ({ errorText, ...props }: Props) => (
+  <View style={styles.container}>
+    <Input
+      style={styles.input}
+      selectionColor={theme.colors.text.primary}
+      underlineColor="transparent"
+      mode="outlined"
+      {...props}
+    />
+    {errorText ? <Text style={styles.error}>{errorText}</Text> : null}
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginVertical: 12,
+  },
+  input: {
+    backgroundColor: theme.colors.bg.primary,
+  },
+  error: {
+    fontSize: 14,
+    color: theme.colors.text.error,
+    paddingHorizontal: 4,
+    paddingTop: 4,
+  },
+});
+
+export default memo(TextInput);
