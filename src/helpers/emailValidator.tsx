2f16af46dee8b1f5b6b50f241f0e659bca4ba37d
samblock <block.sa@northeastern.edu>
2023-04-25 21:41:33 -0400

    starting login screen

---
+++ src/helpers/emailValidator.tsx
@@ -0,0 +1,5 @@
+export function emailValidator(email:any){
+    if(!email) return "email can't be empty";
+    //more to be added here!
+    return '';
+}
---
+++ src/helpers/passwordValidator.tsx
@@ -0,0 +1,5 @@
+export function passwordValidator(password:any){
+    if(!password) return "email can't be empty";
+    //more checks can be added here
+    return '';
+}
