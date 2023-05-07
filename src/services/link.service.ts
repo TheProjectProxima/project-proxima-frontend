09cbd9287e8a524052ca3aa1f29bc60115afbf26
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-05-07 00:41:26 -0400

    Added link service.

---
+++ src/services/link.service.ts
@@ -0,0 +1,17 @@
+import requests from '../api/requests';
+import { GroupLink } from '../lib/types/model';
+import { LoginUser, ResponseUser, SignUpUser } from '../lib/types/request';
+
+export default {
+    getUser: (uuid: string): Promise<GroupLink> =>
+    requests.get(`/group-link/${uuid}`),
+  getAllGroupLinksUser: (): Promise<GroupLink[]> =>
+    requests.get('/users/group-links/'),
+  updateGroupLink: (uuid: string, updatedGroupLink: GroupLink): Promise<GroupLink> =>
+    requests.put(`/group-link/${uuid}`, { groupLink: updatedGroupLink }),
+  deleteGroupLinkUser: (uuid: string): Promise<void> =>
+    requests.delete(`/uusers/group-links/${uuid}`)
+  createGroupLink: (newGroupLink: GroupLink) : Promise<ResponseGroupLink> => {
+    requests.post("/group-link/", {newGroupLink})
+  } 
+};
