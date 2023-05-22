335de9a9369d74fc6d9fae3e189d98969c66065f
Dhivas Sugumar <dhivassugumar@gmail.com>
2023-05-21 23:12:52 -0400

    Trying to get the user login to work with backend.

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 // need to add an env config for local and prod later 
-export const API_URI = 'localhost:3002/'
+export const API_URI = 'https://56d1-2601-197-a7f-cb30-e4-743e-367e-f4cd.ngrok-free.app'
 
 
 // this is for when we add  JWT to it for authentication for every request 
@@ -33,22 +33,22 @@ const handleErrors = (error : any) => {
 const requests = {
   delete: (url : string) =>
     axios
-      .delete(`\${API_URI}\${url}`)
+      .delete(`${API_URI}${url}`)
       .then((response) => response.data)
       .catch(handleErrors),
   get: (url : string) =>
     axios
-      .get(`\${API_URI}\${url}`)
+      .get(`${API_URI}${url}`)
       .then((response) => response.data)
       .catch(handleErrors),
   put: (url : string, body: any) =>
     axios
-      .put(`\${API_URI}\${url}`, body)
+      .put(`${API_URI}${url}`, body)
       .then((response) => response.data)
       .catch(handleErrors),
   post: (url : string, body: any) =>
     axios
-      .post(`\${API_URI}\${url}`, body)
+      .post(`${API_URI}${url}`, body)
       .then((response) => response.data)
       .catch(handleErrors),
 };
